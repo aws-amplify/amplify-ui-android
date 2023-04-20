9892d76094583c13570cef7469355750ac946ee2
Matt Creaser <mattwcc@amazon.com>
2023-04-20 12:14:05 -0300

    Convert build files to kotlin DSL

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    // Set the prefix used for dependencies declared in gradle/libs.versions.toml
+    defaultLibrariesExtensionName.set("dependency")
+}
+
+rootProject.name = "amplify-ui-android"
+include(":liveness")
+include(":authenticator")

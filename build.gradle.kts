0476e5edc3f7547a02ef087591e3d636ee5c9267
Matt Creaser <mattwcc@amazon.com>
2023-05-01 11:44:30 -0300

    Setup build-logic module for convention plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ plugins {
     id("com.android.library") version "7.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10" apply false
+    id("org.jetbrains.kotlin.jvm") version "1.7.21" apply false
 }
 
 val optInAnnotations = listOf(

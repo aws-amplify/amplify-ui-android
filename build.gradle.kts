1291292c7ebed8e740b7a62cb63abdd5a2e0946b
Ankit Shah <22114629+ankpshah@users.noreply.github.com>
2023-11-14 15:27:08 -0800

    chore: Add build-time dependency license check (#88)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,59 @@ buildscript {
         google()
         mavenCentral()
     }
+    dependencies {
+        classpath("app.cash.licensee:licensee-gradle-plugin:1.7.0")
+    }
+}
+
+subprojects {
+    apply(plugin = "app.cash.licensee")
+    configure<app.cash.licensee.LicenseeExtension> {
+        allow("Apache-2.0")
+        allow("MIT")
+        allow("BSD-2-Clause")
+        allowDependency("org.bouncycastle", "bcprov-jdk15on", "1.70") {
+            "MIT License"
+        }
+        allowDependency("org.ow2.asm", "asm", "9.4") {
+            "3-Clause BSD License"
+        }
+        allowDependency("org.ow2.asm", "asm-analysis", "9.4")
+        allowDependency("org.ow2.asm", "asm-commons", "9.4")
+        allowDependency("org.ow2.asm", "asm-tree", "9.4")
+        allowDependency("org.ow2.asm", "asm-util", "9.4")
+        allowDependency("com.ibm.icu", "icu4j", "70.1")
+        allowUrl("http://aws.amazon.com/apache2.0")
+        allowUrl("https://developer.android.com/studio/terms.html")
+
+        ignoreDependencies("javax.annotation", "javax.annotation-api") {
+            "Transitive dependency for androidx.test.espresso:espresso-core"
+        }
+        ignoreDependencies("org.junit", "junit-bom") {
+            because("Unit Testing Dependency")
+        }
+        ignoreDependencies("org.junit", "jupiter") {
+            because("Unit Testing Dependency")
+        }
+        ignoreDependencies("org.junit.jupiter", "junit-jupiter") {
+            because("Unit Testing Dependency")
+        }
+        ignoreDependencies("org.junit.jupiter", "junit-jupiter-params") {
+            because("Unit Testing Dependency")
+        }
+        ignoreDependencies("org.junit", "junit-jupiter-params") {
+            because("Unit Testing Dependency")
+        }
+        ignoreDependencies("org.junit.platform", "junit-platform-commons") {
+            because("Unit Testing Dependency")
+        }
+        ignoreDependencies("org.junit.platform", "junit-platform-engine") {
+            because("Unit Testing Dependency")
+        }
+        ignoreDependencies("junit", "junit") {
+            because("Unit Testing Dependency")
+        }
+    }
 }
 
 // Plugin aliases are a warning in Gradle < 8.1, this suppress can be removed after updating

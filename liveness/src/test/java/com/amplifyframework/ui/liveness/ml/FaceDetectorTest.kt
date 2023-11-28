29fe929ec8702cc45558fef56c971c25b24cac40
Thomas Leing <leint@amazon.com>
2023-11-28 11:29:17 -0800

    lint

--- liveness/src/test/java/com/amplifyframework/ui/liveness/ml/FaceDetectorTest.kt
+++ liveness/src/test/java/com/amplifyframework/ui/liveness/ml/FaceDetectorTest.kt
@@ -30,7 +30,7 @@ internal class FaceDetectorTest {
     @Before
     fun setup() {
         val state = mockk<LivenessState>() {
-            every {faceTargetChallenge} returns null
+            every { faceTargetChallenge } returns null
         }
         detector = FaceDetector(state)
     }
@@ -53,12 +53,14 @@ internal class FaceDetectorTest {
 
         // RectF is stubbed in unit tests, so we need to access what it was called with in a different way
         mockkConstructor(RectF::class)
-        every { constructedWith<RectF>(
-            InvokeMatcher<Float> {rectLeft = it},
-            InvokeMatcher<Float> {rectTop = it},
-            InvokeMatcher<Float> {rectRight = it},
-            InvokeMatcher<Float> {rectBottom = it},
-        ).isEmpty } answers {
+        every {
+            constructedWith<RectF>(
+                InvokeMatcher<Float> { rectLeft = it },
+                InvokeMatcher<Float> { rectTop = it },
+                InvokeMatcher<Float> { rectRight = it },
+                InvokeMatcher<Float> { rectBottom = it },
+            ).isEmpty
+        } answers {
             assertClose(rectLeft, 0.16585289f)
             assertClose(rectTop, 0.04175697f)
             assertClose(rectRight, 0.16585289f + 0.62404186f)
@@ -78,6 +80,6 @@ internal class FaceDetectorTest {
     }
 
     private fun assertClose(actual: Float, expected: Float, difference: Float = 0.000001f) {
-        assert(kotlin.math.abs(actual-expected) < difference)
+        assert(kotlin.math.abs(actual - expected) < difference)
     }
 }

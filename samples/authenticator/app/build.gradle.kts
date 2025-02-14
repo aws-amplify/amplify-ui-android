4aae4d11ef69c22ef613d1b1762ee9ad97c58748
Matt Creaser <mattwcc@amazon.com>
2025-02-14 13:04:56 -0400

    Remember your theme selection in the sample app

--- samples/authenticator/app/build.gradle.kts
+++ samples/authenticator/app/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation(libs.bundles.compose)
     implementation(libs.androidx.lifecycle)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.samples.androidx.datastore.prefs)
 
     coreLibraryDesugaring(libs.android.desugar)
 }

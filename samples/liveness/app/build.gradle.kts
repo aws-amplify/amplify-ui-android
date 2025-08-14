9d4908c599fb105983f3584174361a32048bc95f
Matt Creaser <mattwcc@amazon.com>
2025-08-14 16:32:32 -0300

    Update Liveness sample app to use build-logic

---
+++ samples/liveness/app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.serialization)
+    id("amplify.android.application")
+}
+
+android {
+    namespace = "com.amplifyframework.ui.sample.liveness"
+    defaultConfig {
+        buildConfigField("boolean", "SHOW_DEBUG_UI", "false")
+    }
+}
+
+dependencies {
+    val livenessVersion = "1.6.0"
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation("com.amplifyframework.ui:liveness:$livenessVersion")
+
+    implementation(libs.amplify.core.kotlin)
+    implementation(libs.amplify.api)
+    implementation(libs.amplify.auth)
+    implementation(libs.amplify.predictions)
+
+    implementation(libs.bundles.compose)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.accompanist)
+
+    coreLibraryDesugaring(libs.android.desugar)
+}

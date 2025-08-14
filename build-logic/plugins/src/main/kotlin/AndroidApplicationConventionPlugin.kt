bc5676f9dd9154f18a5647ca3aafadd6563117e6
Matt Creaser <mattwcc@amazon.com>
2025-08-14 15:51:23 -0300

    Update Authenticator Sample app to use build-logic

---
+++ build-logic/plugins/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import com.amplify.ui.configureAndroid
+import com.android.build.api.dsl.ApplicationExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.api.tasks.testing.Test
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.provideDelegate
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
+
+/**
+ * This convention plugin configures an Android library module
+ */
+class AndroidApplicationConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target.pluginManager) {
+            apply("com.android.application")
+            apply("org.jetbrains.kotlin.plugin.compose")
+            apply("org.jetbrains.kotlin.android")
+        }
+
+        with(target) {
+            extensions.configure<ApplicationExtension> {
+                target.configureAndroid(this)
+                defaultConfig {
+                    targetSdk = 34
+                }
+                compileOptions {
+                    isCoreLibraryDesugaringEnabled = true
+                }
+            }
+
+            tasks.withType<JavaCompile>().configureEach {
+                options.compilerArgs.apply {
+                    add("-Xlint:all")
+                    add("-Werror")
+                }
+            }
+
+            tasks.withType<Test>().configureEach {
+                minHeapSize = "128m"
+                maxHeapSize = "4g"
+            }
+
+            configure<KotlinProjectExtension> {
+                jvmToolchain(17)
+            }
+        }
+    }
+}
---
+++ samples/authenticator/app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    id("amplify.android.application")
+}
+
+android {
+    namespace = "com.amplifyframework.ui.sample.authenticator"
+}
+
+dependencies {
+    val authenticatorVersion = "1.6.0"
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation("com.amplifyframework.ui:authenticator:$authenticatorVersion")
+
+    implementation(libs.bundles.compose)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.activity.compose)
+
+    coreLibraryDesugaring(libs.android.desugar)
+}
